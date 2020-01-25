9298ed4a2056cb25ed35635c6277dabaadd06827
Lucas Atayde <lsa4@rice.edu>
2020-01-25 14:14:25 -0600

    Started script to clean the data. Additionally created a columns names file

--- data_prep/clean.py
+++ data_prep/clean.py
@@ -7,5 +7,15 @@ data.
 
 import pandas as pd
 
+usaa = pd.read_csv("data_prep/raw/US_Accidents_Dec19.csv", skiprows=range( int(2974335 * .01), 2974335))
+# usaa = pd.read_csv("data_prep/raw/US_Accidents_Dec19.csv")
+
+# Step 1: Convert accident ids to integers
+usaa["ID"] = usaa["ID"].apply(lambda id: int(id[2:]))
+
+# Drop unnecessary data. Makes computation slower and adds noise
+usaa = usaa.drop(columns="Source")
+
+
 
 
